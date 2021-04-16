f9be21f82ce77aef25431479ca709cc1fe7fb743
david <kontakt@davidbigus.de>
2021-04-16 03:01:30 +0200

    update

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDEvgAtsxyLzlcJL08Wyva3tTHhepT_Cm8",
+  authDomain: "database-sc.firebaseapp.com",
+  databaseURL: "https://database-sc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "database-sc",
+  storageBucket: "database-sc.appspot.com",
+  messagingSenderId: "793281305958",
+  appId: "1:793281305958:web:76676725032d79c83bfca7"
+}
+
+firebase.initializeApp(config);
+const db = firebase.database().ref('todo/');
+
+export default db;
